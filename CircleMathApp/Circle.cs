4195a5fff64b910703bfabf3b4be745ecb1bf21b
Mustafiz <hilaly.mustafiz@hotmail.com>
2015-03-06 13:56:39 +0600

    First Commit

---
+++ CircleMathApp/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleMathApp
+{
+    class Circle
+    {
+        public double GetDiameter(double radius)
+        {
+            return 2 * radius;
+        }
+
+        public double GetPerimeter(double radius)
+        {
+            return 2*Math.PI*radius;
+        }
+
+        public double GetArea(double radius)
+        {
+            return Math.PI*radius*radius;
+        }
+    }
+}
